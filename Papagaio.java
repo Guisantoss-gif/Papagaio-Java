9c21c4c1d519812b53818c8f01d47021da1ebae8
Guisantoss-gif <go9104390@gmail.com>
2026-03-19 21:39:56 -0300

    Add files via upload

---
+++ Papagaio.java
@@ -0,0 +1,19 @@
+
+public class Papagaio
+{
+public static void main(String[] args) {
+
+ 
+
+ if ( args.length == 0 ) { 
+   System.out.println("Você não digitou nada");
+
+   
+ } else{ 
+   System.out.println("Você digitou " +  args[1]); 
+ }
+
+} 
+ }
+
+ 
